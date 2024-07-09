e9ff679537f0a31d67c69436895ac0f4f23eff9d
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2024-07-09 22:53:35 +0300

    update comments

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@
 ========         .----------------------.   | === |          ========
 ========         |.-""""""""""""""""""-.|   |-----|          ========
 ========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
+========         ||   MY NEOVIM CONFIG ||   |-----|          ========
 ========         ||                    ||   | === |          ========
 ========         ||                    ||   |-----|          ========
 ========         ||:Tutor              ||   |:::::|          ========
@@ -20,78 +20,43 @@
 =====================================================================
 =====================================================================
 
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
+    Use `:help lua-guide` as a reference for how Neovim integrates Lua.
     - :help lua-guide
+    - or `<leader>sh` then search `lua-guide` if you want to search the :help through telescope's fuzzy search
     - (or HTML version): https://neovim.io/doc/user/lua-guide.html
 
-Kickstart Guide:
+  TODO: Run the command `:Tutor` in Neovim if you need a quick refersher on how the hell you use this vim thing :)
 
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
+    ALWAYS.READ.THE.HELP.WHEN.YOU.FEEL.LOST
+    `:help` (without providing any other arguments)
     This will open up a help window with some basic information
     about reading, navigating and searching the builtin help documentation.
 
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
+    The help should be the first place you go to look when you're stuck or confused
+    with something. It's one of the best favorite Neovim features.
 
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
+    MOST IMPORTANTLY, don't forget the telescope plugin's keymap "<space>sh" to [s]earch the [h]elp documentation,
+    which is very useful when you're not exactly sure of what you're looking for and you want to "fuzzy search" a thing.
 
-  I have left several `:help X` comments throughout the init.lua
+    There are several `:help X` comments throughout the init.lua
     These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
+    plugins or Neovim features used in this config file.
 
    NOTE: Look for lines like this
 
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
+If you experience any errors while trying to install this neovim configuration, run `:checkhealth` for more info.
 
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+--  So this will be the first thing that we ever config, so that it runs first before anything else.
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- Netrw (neovim's default built-in file browser) settings
 vim.g.netrw_banner = 0 -- Disable the banner
@@ -101,10 +66,9 @@ vim.g.netrw_winsize = 25 -- Set the width of the vertical split
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+--  NOTE: For more options, you can see `:help option-list`
 
--- Make line numbers default
+-- Make line numbers in the gutter visible
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
@@ -116,7 +80,7 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
--- Sync clipboard between OS and Neovim.
+-- Sync clipboard between OS and Neovim's registers.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
@@ -198,7 +162,11 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
-
+-- vim.keymap lets us add functionality to our keystrokes, but vim.api will let us add functionality when particular are fired/triggered
+-- by neovim, this is very similar to event listeners in javascript, but we call them autocommands in neovim, cause they automatically run
+-- some command/callback, the grou parameter helps us prevent duplicate listeners, if you've used js event listeners before and you called
+-- something like registering an event listener twice, you end up running the code twice which is not what we want to do
+-- and groups help prevent that.
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
