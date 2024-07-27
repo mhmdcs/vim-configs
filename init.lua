75c55e0fe5bddaea19a97b2a6277ca9bd034f320
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2024-07-27 18:43:02 +0300

    add more themes

--- init.lua
+++ init.lua
@@ -783,30 +783,72 @@ require('lazy').setup({
       }
     end,
   },
+  -- Themes
+  -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  -- tokyonight
+  --  {
+  --  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --  'folke/tokyonight.nvim',
+  --  priority = 1000, -- Make sure to load this before all the other start plugins.
+  --  init = function()
+  --    -- Load the colorscheme here.
+  --    -- Like many other themes, this one has different styles, and you could load
+  --    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --    vim.cmd.colorscheme 'tokyonight-night'
+  --    -- You can configure highlights by doing something like:
+  --    vim.cmd.hi 'Comment gui=none'
+  --  end,
+  --},
+  -- kanagawa
+  --  {
+  --  'rebelot/kanagawa.nvim',
+  --  priority = 1000, -- Make sure to load this before all the other start plugins.
+  --  init = function()
+  --    vim.cmd.colorscheme 'kanagawa-dragon'
+  --  end,
+  --},
+  -- melange
+  --  {
+  --  'savq/melange-nvim',
+  --  priority = 1000, -- Make sure to load this before all the other start plugins.
+  --  init = function()
+  --    vim.opt.termguicolors = true
+  --    vim.cmd.colorscheme 'melange'
+  --  end,
+  --},
+  -- midnight
+  --  {
+  --  'dasupradyumna/midnight.nvim',
+  --  priority = 1000, -- Make sure to load this before all the other start plugins.
+  --  init = function()
+  --    vim.cmd.colorscheme 'midnight'
+  --  end,
+  --},
+  -- onedark
+  -- {
+  --   'navarasu/onedark.nvim',
+  --   config = function()
+  --     require('onedark').setup {
+  --       style = 'warmer',
+  --     }
+  --     require('onedark').load()
+  --   end,
+  -- },
+  -- github
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup {
+        -- ...
+      }
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    -- 'folke/tokyonight.nvim',
-    -- 'rebelot/kanagawa.nvim',
-    'savq/melange-nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
-      -- vim.cmd.colorscheme 'kanagawa-dragon'
-      vim.opt.termguicolors = true
-      vim.cmd.colorscheme 'melange'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd 'colorscheme github_dark_default'
     end,
   },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
